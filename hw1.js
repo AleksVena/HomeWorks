d8969b35fe25ce2e9bab68681f7b29ce96220f5e
AleksVena <skmk822@gmail.com>
2021-04-13 23:21:05 +0600

    fix bugs

--- hw1.js
+++ hw1.js
@@ -1,79 +1,51 @@
-class ok {
-    name;
-    count;
-}
-
 function maxItemAssotiation() {
     //const startArray = [["e", "d"], ["a", "b"], ["a", "c"], ["d", "e"], ["e", "a"] ];
-    const startArray = [["a", "b"], ["a", "c"], ["d", "e"]];
+    //const startArray = [["a", "b"], ["a", "c"], ["d", "e"]];
+    let startArray = [
+        ['d', 'e'],
+        ['e', 'n'],
+        ['a', 'b'],
+        ['a', 'c'],
+        ['c', 'q'],
+        ['x', 'r']
+    ];
 
-    let countElement = [];
-    const finalList = [];
-    startArray.forEach(element => {
-        element.forEach(x => {
-            let el = countElement.find(item => item.name == x);
-            if (!el) {
-                const r = new ok();
-                r.name = x;
-                r.count = 1;
-                countElement.push(r);
-            }
-            else {
-                el.count++;
-            }
-        });
-    });
+    const finalArray = [];
 
-    
-
-    countElement.sort((a, b) => b.count - a.count);
-
-    const fe = countElement[0];
-    finalList.push(fe);
-
-    countElement = countElement.slice(1, countElement.length);
-
-    while (countElement.find(item => item.count == fe.count)) {
-        const se = countElement[0];
-        finalList.push(se);
-        countElement = countElement.slice(1, countElement.length);
+    while (startArray.length > 0) {
+       const req = [];
+       const r = startArray[0];
+       req.push(...r);
+       startArray.splice(0, 1);
+       let i = 0;
+       while (i < req.length){
+           const t = req[i];
+           startArray.forEach(x => {
+               if(x.indexOf(t) > -1){
+                   x.forEach(r => {
+                       if(req.indexOf(r) == -1){
+                           req.push(r);
+                       }
+                   });
+                   startArray.splice(x.indexOf(t), 1);
+               }
+           });
+           i++;
+       }
+       req.sort((a, b) => a.localeCompare(b));
+       if (req.length > 0)
+       finalArray.push(req);
     }
 
-    finalList.sort((a, b) => a.name.localeCompare(b.name));
+    finalArray.sort((a, b) => b.length - a.length );
+    
+    if (finalArray.length > 0){
+        console.log('Ответ', finalArray[0])
+        if (finalArray.length > 1){
+            console.log('Остальное', finalArray.slice(1));
+        }
+    }
 
-    const feLast = finalList[0];
-
-    const firstGroup = [];
-    const secondGroup = [];
-
-    startArray.forEach(element => {
-        element.forEach(e => {
-            if (e == feLast.name) {
-                //console.log(element);   
-                element.forEach(r => {
-                    if (firstGroup.findIndex(i => i == r) == -1) {
-                        firstGroup.push(r);
-                    }
-                });
-            }
-        });
-    });
-
-    startArray.forEach(element => {
-        element.forEach(e => {
-            if (firstGroup.findIndex(i => i == e) == -1) {
-                if (secondGroup.findIndex(i => i == e) == -1) {
-                    secondGroup.push(e);
-                }
-            }
-        });
-    });
-
-    firstGroup.sort((a, b) => a.localeCompare(b));
-    secondGroup.sort((a, b) => a.localeCompare(b));
-
-    console.log("Ответ:", firstGroup);
-    console.log("Группа 2:", secondGroup);
 }
 
 maxItemAssotiation();
