19961b2ec77dad6c24caba27fb35025317c47f3e
AleksVena <skmk822@gmail.com>
2021-07-26 18:41:53 +0600

    fix: added tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render Wind:', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Wind:/i)).toBeInTheDocument();
 });
